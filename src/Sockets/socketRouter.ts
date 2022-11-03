da071878352e9b699f2d0dfc06a75f183d5772cc
Ivan Luk <luk.ivan.mech@gmail.com>
2022-11-02 19:24:58 -0700

    Set up file structure for notifications

--- src/Sockets/socketRouter.ts
+++ src/Sockets/socketRouter.ts
@@ -1,6 +1,13 @@
 import { Socket } from 'socket.io';
 import io from '../index';
+import * as messageController from './sControllers/messages';
+import * as notifController from './sControllers/notifications';
+import { notif, messages } from '../Utilities/Strings/sockets';
 
-export async function onConnection (socket: Socket ) {
+export async function onConnection (socket: Socket) {
   console.log('Socket Connected')
+
+  socket.on(messages.example, messageController.example);
+
+  socket.on(notif.notifExample, notifController.notifExample);
 }
