5135da28b16f951766e3b44508ecf8c72c8ab2ae
mfitz6022 <mfitz6022@gmail.com>
2022-11-08 17:19:59 -0800

    server side sockets finished

--- build/Sockets/sControllers/messages.js
+++ build/Sockets/sControllers/messages.js
@@ -1,20 +1,21 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.leave = exports.join = exports.send = void 0;
-function send(messageData, socket) {
-    console.log(`the following data was recieved from user: ${messageData.username}: ${messageData} \n the recipient(s) are in chatroom: ${messageData.chatId}`);
-    socket.broadcast.to(messageData.chatId).emit('receive_message', messageData);
+function send(messageData) {
+    console.log(`the following data was recieved from user: ${messageData.username}: ${messageData} \nthe recipient(s) are in chatroom: ${messageData.chatId}`);
+    this.emit('receive_message', messageData);
+    // this.broadcast.to(messageData.chatId).emit('receive_message', messageData);
 }
 exports.send = send;
-function join(userData, socket) {
+function join(userData) {
     console.log(`${userData.username} has joined the following rooms: ${userData.rooms}`);
-    socket.join(userData.rooms);
+    this.join(userData.rooms);
 }
 exports.join = join;
-function leave(userData, socket) {
+function leave(userData) {
     const rooms = userData.rooms;
     rooms.forEach((room) => {
-        socket.leave(room);
+        this.leave(room);
     });
     console.log(`${userData.username} has left the following rooms: ${userData.rooms}`);
 }
--- src/Sockets/sControllers/messages.ts
+++ src/Sockets/sControllers/messages.ts
@@ -2,20 +2,21 @@ import { Socket } from "socket.io";
 import { Messages } from "../../Utilities/Interfaces/Chats";
 import { User } from "../../Utilities/Strings/interfaces/Chats";
 
-export function send (messageData: Messages, socket: Socket): void {
-  console.log(`the following data was recieved from user: ${messageData.username}: ${messageData} \n the recipient(s) are in chatroom: ${messageData.chatId}`);
-  socket.broadcast.to(messageData.chatId).emit('receive_message', messageData);
+export function send (this: Socket, messageData: Messages): void {
+  console.log(`the following data was recieved from user: ${messageData.username}: ${messageData} \nthe recipient(s) are in chatroom: ${messageData.chatId}`);
+  this.emit('receive_message', messageData);
+  // this.broadcast.to(messageData.chatId).emit('receive_message', messageData);
 }
 
-export function join (userData: User, socket: Socket): void {
+export function join (this: Socket, userData: User): void {
   console.log(`${userData.username} has joined the following rooms: ${userData.rooms}`);
-  socket.join(userData.rooms);
+  this.join(userData.rooms);
 }
 
-export function leave (userData: User, socket: Socket): void {
+export function leave (this: Socket, userData: User): void {
   const rooms: string[] = userData.rooms;
   rooms.forEach((room: string): void => {
-    socket.leave(room)
+    this.leave(room)
   });
   console.log(`${userData.username} has left the following rooms: ${userData.rooms}`);
 }
